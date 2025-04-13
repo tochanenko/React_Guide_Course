be5e1556731a33f0c740f931575768844220a5f4
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-04-13 14:11:25 +0300

    Style components using styled-components plugin

--- 06-styling-project/src/components/AuthInputs.jsx
+++ 06-styling-project/src/components/AuthInputs.jsx
@@ -1,5 +1,35 @@
 import { useState } from 'react';
 
+import {styled} from 'styled-components';
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
+  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -22,26 +52,28 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <StyledDiv>
         <p>
-          <label>Email</label>
-          <input
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
+            $invalid={emailNotValid}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
+            $invalid={passwordNotValid}
           />
         </p>
-      </div>
+      </StyledDiv>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
--- 06-styling-project/src/components/Header.jsx
+++ 06-styling-project/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1 className={classes.paragraph}>ReactArt</h1>
+      <p style={{color:"red"}}>A community of artists and art-lovers.</p>
     </header>
   );
 }

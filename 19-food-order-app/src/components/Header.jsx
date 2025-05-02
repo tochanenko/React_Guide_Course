f3c63d66ab856ca6c99ba009662ba88a33ab0644
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-05-02 20:58:19 +0300

    Finish Cart Context Implementation

---
+++ 19-food-order-app/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
+import CartContext from '../store/CartContext';
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => totalNumberOfItems + item.quantity, 0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant"/>
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({ totalCartItems })</Button>
+      </nav>
+    </header>
+  );
+}

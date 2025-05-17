c54b2835243036293ea562c7dbb14842057a7a41
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-05-17 10:56:47 +0300

    Finish nextjs demo app

---
+++ 26-nextjs-basics/app/blog/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function PlogPage() {
+  return <main>
+    <h1>The Blog</h1>
+    <Link href="/blog/post-1">Post 1</Link>
+    <Link href="/blog/post-2">Post 1</Link>
+  </main>
+}

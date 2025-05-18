52b75505db9a04d9318656be0dc79a29e40de593
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-05-18 20:19:43 +0300

    Finish Patterns project

--- 29-patterns/src/App.jsx
+++ 29-patterns/src/App.jsx
@@ -1,4 +1,45 @@
 import Accordion from "./components/Accordion/Accordion";
+import SearchableList from "./components/SearchableList/SearchableList";
+import Place from './components/Place';
+
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return <main>
@@ -23,6 +64,17 @@ function App() {
           <Accordion.Content className="accordion-item-content">We hire most experinced local guides.</Accordion.Content>
         </Accordion.Item>
       </Accordion>
+    </section>
+    <section>
+      <SearchableList
+        items={PLACES}
+        itemKeyFn={(item) => item.id}
+      >{(item) => <Place item={item} />}</SearchableList>
+
+      <SearchableList
+        items={["one", "two"]}
+        itemKeyFn={(item) => item}
+      >{(item) => item}</SearchableList>
     </section>
   </main>;
 }
